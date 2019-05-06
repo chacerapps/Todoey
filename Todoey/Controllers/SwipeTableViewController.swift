293ccfa5b557f0affb7f93ab145c8278be101eab
chacer <chacersupport@gmail.com>
2019-05-06 11:31:18 -0500

    ChameleonFramework/Swift - random colored cells

---
+++ Todoey/Controllers/SwipeTableViewController.swift
@@ -0,0 +1,53 @@
+//
+//  SwipeTableViewController.swift
+//  Todoey
+//
+//  Created by ccztr on 5/4/19.
+//  Copyright © 2019 Chacer. All rights reserved.
+//  Updated 5/05/2019
+
+import UIKit
+import SwipeCellKit
+
+class SwipeTableViewController: UITableViewController, SwipeTableViewCellDelegate {
+    var cell: UITableViewCell?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.rowHeight = 80.0
+    }
+    
+    //TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! SwipeTableViewCell
+        cell.delegate = self
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+        guard orientation == .right else { return nil }
+        
+        let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
+            print("Delete Cell at: \(indexPath)")
+            self.updateModel(at: indexPath)
+        }
+        // customize the action appearance
+        deleteAction.image = UIImage(named: "delete-icon")
+        
+        return [deleteAction]
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeTableOptions {
+        var options = SwipeTableOptions()
+        options.expansionStyle = .destructive
+        return options
+    }
+
+    // Required here for editActionsForRowAt indexPath but overriden in TodoListViewController & CategoryViewController - this code not run!
+    func updateModel(at indexPath: IndexPath) {
+        // Update our data model index number before commiting to Realm.
+        print("SwipeTable Category/Item deleted from superclass")
+    }
+}
